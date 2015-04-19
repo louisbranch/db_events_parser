a7ea5acf3560323146ac8012075206cdc86b5473
Luiz Branco <me@luizbranco.com>
2015-04-18 20:05:08 -0400

    Extract key/value pairs

--- lib/parser.rb
+++ lib/parser.rb
@@ -10,7 +10,39 @@ class Parser
     inner_content = /^\s*\{(.*?)\}\s*$/mx
     match = blob.match(inner_content)
     raise "Invalid data structure syntax" unless match
-    match[1]
+    extract_key_values(match[1])
+  end
+
+  private
+
+  def extract_key_values(text)
+    struct = {}
+    token_index = 0
+    current_key = nil
+    text.each_char.with_index do |char, i|
+      if char == ':'
+        if current_key
+          raise "Unexpected ':' near '...#{text[i-5..i+5]}...'"
+        else
+          current_key = text[token_index..i-1]
+          token_index = i + 1
+        end
+      elsif char == ','
+        value = text[token_index..i-1].strip
+        if current_key && value.length > 0
+          struct[current_key.strip] = value
+          current_key = nil
+          token_index = i + 1
+        else
+          raise "Unexpected ',' near '...#{text[i-5..i+5]}...'"
+        end
+      elsif i == text.length - 1
+        if current_key
+          struct[current_key.strip] = text[token_index..i].strip
+        end
+      end
+    end
+    struct
   end
 
 end
--- spec/lib/parser_spec.rb
+++ spec/lib/parser_spec.rb
@@ -20,16 +20,32 @@ EOF
     context "when struct synxtax is invalid" do
       let(:blob) { "{date: 2014-01-01, a: 5, b:1" }
 
-      it "raises an error" do
+      it "raises a struct syntax error" do
         expect{subject.parse_struct}.to raise_error("Invalid data structure syntax")
       end
 
     end
 
+    context "when key is missing" do
+      let(:blob) { "{2014-01-01, a: 5, b:1}" }
+
+      it "raises a comma syntax error" do
+        expect{subject.parse_struct}.to raise_error("Unexpected ',' near '...01-01, a: 5...'")
+      end
+    end
+
+    context "when value is missing" do
+      let(:blob) { "{date: 2014-01-01, a: , b:1}" }
+
+      it "raises a colon syntax error" do
+        expect{subject.parse_struct}.to raise_error("Unexpected ',' near '..., a: , b:1...'")
+      end
+    end
+
     context "when syntax is valid" do
       let(:blob) { "{date: 2014-01-01, a: 5, b:1}" }
 
-      xit "split key/value pairs" do
+      it "splits key/value pairs" do
         expect(subject.parse_struct).to eq({
           'date' => '2014-01-01',
           'a' => '5',
